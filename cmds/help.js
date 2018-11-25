38da4f8df30aefbccfa0f4dd847db1a4ed10b7d2
kdbeall <10102694+kdbeall@users.noreply.github.com>
2018-11-25 10:33:54 -0500

    Initialized project structure.

---
+++ cmds/help.js
@@ -0,0 +1,17 @@
+const menus = {
+    main: `
+      cconvert [command] <options>
+  
+      convert .............. convert between currencies
+      history ............ show conversion history
+      version ............ show package version
+      help ............... show help menu for a command`,
+}
+  
+module.exports = (args) => {
+  const subCmd = args._[0] === 'help'
+      ? args._[1]
+      : args._[0]
+  
+  console.log(menus[subCmd] || menus.main)
+  }
